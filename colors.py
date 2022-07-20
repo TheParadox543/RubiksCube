2c59330cb675833accab9dd3e326f8bd89aac066
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-07-20 22:56:08 +0530

    Create colors.py
    Store the rgb values for different colors.

---
+++ colors.py
@@ -0,0 +1,10 @@
+class Colors:
+    """To store the colors for the cube."""
+    def __init__(self):
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.red = (255, 0, 0)
+        self.blue = (0, 0, 255)
+        self.green = (0, 128, 0)
+        self.yellow = (255, 255, 0)
+        self.orange = (255, 165, 0)
