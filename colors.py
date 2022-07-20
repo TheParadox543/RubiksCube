dac596ed7bdd7697c429f5775fec9f9ddc1be79b
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-07-20 23:24:37 +0530

    first face rendered
    Created a 3X3 square to test if it can be rendered.

--- colors.py
+++ colors.py
@@ -1,10 +1,12 @@
 class Colors:
     """To store the colors for the cube."""
     def __init__(self):
-        self.black = (0, 0, 0)
-        self.white = (255, 255, 255)
-        self.red = (255, 0, 0)
-        self.blue = (0, 0, 255)
-        self.green = (0, 128, 0)
-        self.yellow = (255, 255, 0)
-        self.orange = (255, 165, 0)
+        self.colors:dict[str, tuple[int, int, int]] = {
+            "black": (0, 0, 0),
+            "white": (255, 255, 255),
+            "red": (255, 0, 0),
+            "blue": (0, 0, 255),
+            "green": (0, 128, 0),
+            "yellow": (255, 255, 0),
+            "orange": (255, 165, 0),
+        }
--- cube.py
+++ cube.py
@@ -1,6 +1,9 @@
 import sys
+import random
 
 import pygame
+
+from colors import Colors
 
 class Cube:
     """A simulation of a rubik's cube."""
@@ -9,6 +12,15 @@ class Cube:
         """Initialize the cube."""
         pygame.init()
         self.screen = pygame.display.set_mode((1300, 700))
+        self.screen_rect = self.screen.get_rect()
+
+        self.colors = Colors()
+
+        self.face1 = [
+            ["white", "yellow", "blue"],
+            ["red", "green", "orange"],
+            ["blue", "blue", "white"]
+        ]
 
     def run_game(self):
         """Start the window and keep it running."""
@@ -22,9 +34,23 @@ class Cube:
             if event.type == pygame.QUIT:
                 sys.exit()
 
+    def _create_cube(self):
+        """Create cube."""
+        pos_i, pos_j = 0, 0
+        for i in range(50, 200, 50):
+            for j in range(50, 200, 50):
+                cube = pygame.Rect(i, j, 49, 49)
+                now = self.face1[pos_j][pos_i]
+                color_now = self.colors.colors[now]
+                pygame.draw.rect(self.screen, color_now, cube)
+                pos_j += 1
+            pos_i += 1
+            pos_j = 0
+
     def _update_screen(self):
         """Update the screen and flip to the new screen."""
         self.screen.fill((200, 200, 200))
+        self._create_cube()
         pygame.display.flip()
 
 if __name__ == "__main__":
